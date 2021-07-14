f8c4ee6445fa882aa2cd721b98b2ea89517e9821
Alberto Diaz <alberto.diaz05@gmail.com>
2021-07-13 21:04:41 -0400

    v1.1

--- api/app.js
+++ api/app.js
@@ -24,6 +24,7 @@ app.get('/api/entities/:entityCode', async (req, res) => {
             return res.status(200).json(JSON.parse(body));
         });
     } catch (err) {
+        console.error("lala"+err);
         const error = {
             msg: 'Error al listar'
         }
