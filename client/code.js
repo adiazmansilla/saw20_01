9bb606484bef67210b6e882fe977fe4579c26fe8
Alberto Diaz <alberto.diaz05@gmail.com>
2021-07-13 23:00:27 -0400

    v1.5 Se agrega titulo dinamico de la entidad y nombres de anotaciones.

--- client/code.js
+++ client/code.js
@@ -11,21 +11,39 @@ function fillEntities(req) {
     getEntities(req).then(data => {
         //console.log(data.entities);
         const ulEntities = document.getElementById("entities");
-        const ulLabels = document.getElementById("labels");
+        const ulLabels = document.getElementById("claims");
         
         for(var attributename in data.entities[req]){
             const liEntity = document.createElement("li");
             const text = document.createTextNode(attributename+": "+data.entities[req][attributename]);
             liEntity.appendChild(text);
-            ulEntities.appendChild(liEntity);
+            //ulEntities.appendChild(liEntity);
+            document.getElementById('country-code').innerHTML=req;
 
-            if (attributename == 'labels'){
-                const labelsArray =  data.entities[req][attributename];
-                for(var label in labelsArray){
+            if(attributename == 'labels'){
+                const labelsArray =  data.entities[req][attributename]
+                for(let label in labelsArray){
+                    if(label =='es'){
+                        const countryName = labelsArray[label]['value'];
+                        document.getElementById('country-name').innerHTML=countryName;
+                        console.log(countryName);
+                    }
+                }
+
+
+            }
+
+            if (attributename == 'claims'){
+
+                const claimsArray =  data.entities[req][attributename];
+                for(var claim in claimsArray){
+
+                    console.log(JSON.stringify(claim));
+
                     const liLabel = document.createElement("li");
-                    const textLabel = document.createTextNode(label+": "+labelsArray[label]['value']);
+                    const textLabel = document.createTextNode(claim);
                     liLabel.appendChild(textLabel);
-                    ulLabels.appendChild(liLabel);        
+                    ulLabels.appendChild(liLabel);
                 }
             }
         }
