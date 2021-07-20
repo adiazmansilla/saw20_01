8720b02adf6b745f349575d5a2d00e0871116027
Alberto Diaz <alberto.diaz05@gmail.com>
2021-07-19 23:51:13 -0400

    Actualización de front, se obtiene datos de equipo ganador

--- client/code.js
+++ client/code.js
@@ -8,12 +8,18 @@ function fillEntities(req) {
    
     getEntities(req).then(data => {
         const ulLabels = document.getElementById("claims");
-        
+
+        //Obtiene el id del equipo ganador
+        var winnerId = data.entities[req]['claims']['P1346'][0]['mainsnak']['datavalue']['value']['id'];
+        getWinnerTeamName(winnerId);
+
+        console.log(winnerId);
+
         for(let attributename in data.entities[req]){
             document.getElementById('country-code').innerHTML=req;
             //document.getElementById('ca-winner').innerHTML=req;
 
-            //Obtiene el nombre del pais
+            //Obtiene el nombre del evento
             if(attributename === 'labels'){
                 const labelsArray =  data.entities[req][attributename]
                 for(let label in labelsArray){
@@ -25,14 +31,41 @@ function fillEntities(req) {
                 }
             }
 
-            //Obtiene listado de anotaciones
-            if (attributename === 'claims'){
-                const claimsArray =  data.entities[req][attributename];
-                for(let claim in claimsArray){
 
+
+
+            if (attributename === 'claims'){
+                let claimsArray =  data.entities[req][attributename];
+                for(let claim in claimsArray){
                     if(claim==='P1346'){
-                        console.log(JSON.stringify(claim));
-                       // document.getElementById('country-name').innerHTML=claim[mainsnak][datavalue][id];
+                        // let winnerArray =  data.entities[req][attributename][claim];
+                        // let win = winnerArray[0];
+                        // for(const key in win){
+                        //     if(key==='mainsnak'){
+                        //         let mainsnakObj =  data.entities[req][attributename][claim][0][key];
+                        //         for(let dataval in mainsnakObj){
+                        //             if(dataval==='datavalue'){
+                        //                 let winnerValue =  data.entities[req][attributename][claim][0][key][dataval];
+                        //                 for(const wv in winnerValue){
+                        //                     if(wv === 'value'){
+                        //                         let winnerValuevalue =  data.entities[req][attributename][claim][0][key][dataval][wv];
+                        //                         for( let wvv in winnerValuevalue){
+                        //                             if(wvv === 'id'){
+                        //                                 let winnerValueid =  data.entities[req][attributename][claim][0][key][dataval][wv][wvv];
+                        //                                 console.log(JSON.stringify(winnerValueid));
+                        //                             }
+                        //                         }
+                        //
+                        //                     }
+                        //                 }
+                        //
+                        //             }
+                        //
+                        //         }
+                        //
+                        //     }
+                        // }
+
                     }
 
                     const liLabel = document.createElement("tr");
@@ -42,6 +75,15 @@ function fillEntities(req) {
                 }
             }
         }
+    })
+}
+
+function getWinnerTeamName(req) {
+    getEntities(req).then(data => {
+        console.log('***** WINNER TEAM *****');
+        console.log(JSON.stringify(data.entities[req]['labels']['es']['value']));
+        console.log('***************');
+        window.stop();
     })
 }
 
@@ -55,5 +97,5 @@ function pass(ent, country, year){
 function loadImage(){
     var cdg = sessionStorage.getItem("entityCode");
     var cdgImg = '/images/'+cdg+'.jpg';
-    $("img#Myimg").attr('src',cdgImg);
+   $("img#Myimg").attr('src',cdgImg);
 }
